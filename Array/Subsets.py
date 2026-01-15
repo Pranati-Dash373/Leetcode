0c9050846d24f6f1ebddb0b21bd5e9277b928997
Pranati-Dash373 <pranatidash727@gmail.com>
2026-01-15 15:02:40 +0530

    ...

---
+++ Array/Subsets.py
@@ -0,0 +1,35 @@
+'''
+Given an integer array nums of unique elements, return all possible (the power set).
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+
+    1 <= nums.length <= 10
+    -10 <= nums[i] <= 10
+    All the numbers of nums are unique.
+
+'''
+
+class Solution:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        n = len(nums)
+        subset = 1 << n
+        ans = []
+        for i in range(subset):
+            list = []
+            for j in range(n):
+                if i & 1 << j :
+                    list.append(nums[j])
+            ans.append(list)
+        return ans
---
+++ Stack/Remove_k_digits.py
@@ -0,0 +1,46 @@
+'''
+Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
+
+ 
+
+Example 1:
+
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+Example 2:
+
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+Example 3:
+
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+ 
+
+Constraints:
+
+1 <= k <= num.length <= 105
+num consists of only digits.
+num does not have any leading zeros except for the zero itself.
+
+
+'''
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        res = []
+        if len(num) == k :
+            return "0"
+        for i in num:
+            while res and len(num) > 0 and k and res[-1] > i :
+                res.pop()
+                k -= 1
+            res.append(i)
+
+        if k > 0 :
+            res = res[ : -k]
+        
+        return "".join(res).lstrip("0") or "0"
