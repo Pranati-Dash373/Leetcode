aae8f3fe83874721aede1b2222f00232e722ba6e
Pranati-Dash373 <pranatidash727@gmail.com>
2026-01-28 21:50:48 +0530

    ....

---
+++ Greedy algorithm/Valid_parenthesis_string.py
@@ -0,0 +1,55 @@
+'''
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+    Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+    Any right parenthesis ')' must have a corresponding left parenthesis '('.
+    Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+    '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+
+Input: s = "(*)"
+Output: true
+
+Example 3:
+
+Input: s = "(*))"
+Output: true
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 100
+    s[i] is '(', ')' or '*'.
+
+'''
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        min = 0 
+        max = 0
+        for i in s :
+            if i == '(':
+                min += 1
+                max += 1
+            elif i == ')':
+                min -= 1
+                max -= 1
+            else:
+                min -= 1
+                max += 1
+            if min < 0 :
+                min = 0
+            if max < 0 :
+                return False
+        return min == 0 
