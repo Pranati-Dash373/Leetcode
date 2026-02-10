0397b7b1d7ce3441df85553c6aca3cd3c24564fa
Pranati-Dash373 <pranatidash727@gmail.com>
2026-02-11 02:12:58 +0530

    ...

--- Heap/merge_sorted_list.py
+++ Heap/merge_sorted_list.py
@@ -17,33 +17,35 @@ Explanation: The linked-lists are:
 ]
 merging them into one sorted linked list:
 1->1->2->3->4->4->5->6
+
 Example 2:
 
 Input: lists = []
 Output: []
+
 Example 3:
 
 Input: lists = [[]]
 Output: []
+
  
 
 Constraints:
 
-k == lists.length
-0 <= k <= 104
-0 <= lists[i].length <= 500
--104 <= lists[i][j] <= 104
-lists[i] is sorted in ascending order.
-The sum of lists[i].length will not exceed 104.
+    k == lists.length
+    0 <= k <= 104
+    0 <= lists[i].length <= 500
+    -104 <= lists[i][j] <= 104
+    lists[i] is sorted in ascending order.
+    The sum of lists[i].length will not exceed 104.
+
 
 '''
 
-# Definition for singly-linked list.
-
 from typing import Optional
+import heapq
 
-from typing import heapq
-
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
